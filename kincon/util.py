2ebfa20ffd0468dbec4620d70309c7eab5cefffb
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-07-30 23:20:14 +0200

    randomly generate quaternions

--- kincon/util.py
+++ kincon/util.py
@@ -63,3 +63,30 @@ def quaternion_to_rotation_matrix(q):
         return R[0]
     else:
         return R
+
+def random_quaternion(size=1):
+    """
+    Randomly sample a Quaternion from a distribution uniform in
+    3D rotation angles.
+
+    https://www-preview.ri.cmu.edu/pub_files/pub4/kuffner_james_2004_1/kuffner_james_2004_1.pdf
+
+    Parameters
+    ----------
+    size : int
+        Number of quaternions to randomly sample.
+
+    """
+
+    s = np.random.uniform(size=size)
+    s1 = np.sqrt(1 - s)
+    s2 = np.sqrt(s)
+    t1 = np.random.uniform(0, 2*np.pi, size=size)
+    t2 = np.random.uniform(0, 2*np.pi, size=size)
+
+    w = np.cos(t2)*s2
+    x = np.sin(t1)*s1
+    y = np.cos(t1)*s1
+    z = np.sin(t2)*s2
+
+    return np.vstack((w,x,y,z))
