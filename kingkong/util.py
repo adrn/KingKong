113a94d9c96bb506dc3f2d0e359fd0a01d640178
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-08-03 16:34:19 +0200

    made quaternion an object

--- kingkong/util.py
+++ kingkong/util.py
@@ -6,87 +6,113 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os
-import sys
-
 # Third-party
 import numpy as np
-from astropy import log as logger
-import astropy.units as u
 
-# Project
-# ...
+__all__ = ['Quaternion']
 
-__all__ = ['']
-
-def quaternion_to_rotation_matrix(q):
+class Quaternion(object):
     """
-    Convert a Quaternion, ``q``, to a rotation matrix.
-
-    http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/
+    Quaternions for 3D rotations.
 
     Parameters
     ----------
-    q : array_like
-        A quaternion or array of quaternions. Assumes q=(w,x,y,z).
-
-    Returns
-    -------
-    R : :class:`numpy.ndarray`
-        A 3x3 rotation matrix, or an array of such matrices.
+    wxyz : array_like
+        A quaternion or array of quaternions. Ordere is assumed to be ``(w,x,y,z)``.
     """
+    def __init__(self, wxyz):
+        self.wxyz = np.atleast_2d(wxyz).astype(float)
 
-    q = np.array(q)
-    _initial_ndim = q.ndim
-    q = np.atleast_2d(q)
-    qw,qx,qy,qz = q.T
+    @classmethod
+    def from_v_theta(cls, v, theta):
+        """
+        Construct quaternion from unit vector v and rotation angle theta
+        """
+        theta = np.asarray(theta)
+        v = np.asarray(v)
 
-    # number of input quaternions
-    n = q.shape[0]
+        s = np.sin(0.5 * theta)
+        c = np.cos(0.5 * theta)
+        vnrm = np.sqrt(np.sum(v * v))
 
-    R = np.zeros((n,3,3))
+        q = np.concatenate([[c], s * v / vnrm])
+        return cls(q)
 
-    R[:,0,0] = 1 - 2*qy**2 - 2*qz**2
-    R[:,0,1] = 2*qx*qy - 2*qz*qw
-    R[:,0,2] = 2*qx*qz + 2*qy*qw
+    def __repr__(self):
+        return "Quaternion:\n" + self.q.__repr__()
 
-    R[:,1,0] = 2*qx*qy + 2*qz*qw
-    R[:,1,1] = 1 - 2*qx**2 - 2*qz**2
-    R[:,1,2] = 2*qy*qz - 2*qx*qw
+    def __str__(self):
+        return "Quaternion"
 
-    R[:,2,0] = 2*qx*qz - 2*qy*qw
-    R[:,2,1] = 2*qy*qz + 2*qx*qw
-    R[:,2,2] = 1 - 2*qx**2 - 2*qy**2
+    def __mul__(self, other):
+        # multiplication of two quaternions.
+        prod = self.wxyz[:, None] * other.wxyz
+        return self.__class__([(prod[0, 0] - prod[1, 1]
+                                 - prod[2, 2] - prod[3, 3]),
+                                (prod[0, 1] + prod[1, 0]
+                                 + prod[2, 3] - prod[3, 2]),
+                                (prod[0, 2] - prod[1, 3]
+                                 + prod[2, 0] + prod[3, 1]),
+                                (prod[0, 3] + prod[1, 2]
+                                 - prod[2, 1] + prod[3, 0])])
+    @property
+    def v_theta(self):
+        """
+        Return the ``(v, theta)`` equivalent of the (normalized) quaternion.
+        """
+        # compute theta
+        norm = np.sqrt((self.wxyz ** 2).sum(0))
+        theta = 2 * np.arccos(self.wxyz[0] / norm)
 
-    if _initial_ndim == 1:
-        return R[0]
-    else:
-        return R
+        # compute the unit vector
+        v = np.array(self.wxyz[1:], order='F', copy=True)
+        v /= np.sqrt(np.sum(v ** 2, 0))
 
-def random_quaternion(size=1):
-    """
-    Randomly sample a Quaternion from a distribution uniform in
-    3D rotation angles.
+        return v, theta
 
-    https://www-preview.ri.cmu.edu/pub_files/pub4/kuffner_james_2004_1/kuffner_james_2004_1.pdf
+    @property
+    def rotation_matrix(self):
+        """
+        Return the rotation matrix of the (normalized) quaternion
+        """
+        v, theta = self.v_theta()
+        c = np.cos(theta)
+        s = np.sin(theta)
 
-    Parameters
-    ----------
-    size : int
-        Number of quaternions to randomly sample.
+        return np.array([[v[0] * v[0] * (1. - c) + c,
+                          v[0] * v[1] * (1. - c) - v[2] * s,
+                          v[0] * v[2] * (1. - c) + v[1] * s],
+                         [v[1] * v[0] * (1. - c) + v[2] * s,
+                          v[1] * v[1] * (1. - c) + c,
+                          v[1] * v[2] * (1. - c) - v[0] * s],
+                         [v[2] * v[0] * (1. - c) - v[1] * s,
+                          v[2] * v[1] * (1. - c) + v[0] * s,
+                          v[2] * v[2] * (1. - c) + c]])
 
-    """
+    @classmethod
+    def random(cls, size=1):
+        """
+        Randomly sample a Quaternion from a distribution uniform in
+        3D rotation angles.
 
-    s = np.random.uniform(size=size)
-    s1 = np.sqrt(1 - s)
-    s2 = np.sqrt(s)
-    t1 = np.random.uniform(0, 2*np.pi, size=size)
-    t2 = np.random.uniform(0, 2*np.pi, size=size)
+        https://www-preview.ri.cmu.edu/pub_files/pub4/kuffner_james_2004_1/kuffner_james_2004_1.pdf
 
-    w = np.cos(t2)*s2
-    x = np.sin(t1)*s1
-    y = np.cos(t1)*s1
-    z = np.sin(t2)*s2
+        Parameters
+        ----------
+        size : int
+            Number of quaternions to randomly sample.
 
-    return np.vstack((w,x,y,z)).T
+        """
+
+        s = np.random.uniform(size=size)
+        s1 = np.sqrt(1 - s)
+        s2 = np.sqrt(s)
+        t1 = np.random.uniform(0, 2*np.pi, size=size)
+        t2 = np.random.uniform(0, 2*np.pi, size=size)
+
+        w = np.cos(t2)*s2
+        x = np.sin(t1)*s1
+        y = np.cos(t1)*s1
+        z = np.sin(t2)*s2
+
+        return cls(np.vstack((w,x,y,z)).T)
